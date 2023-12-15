e352d41b1e7a81d773fc4e1e4920b3549592c14f
Caio Barilli <caiobarilli@gmail.com>
2023-12-15 17:32:18 -0300

    feat: translate app to pt br

--- laravel/resources/views/auth/forgot-password.blade.php
+++ laravel/resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Esqueceu sua senha? Não há problema. Basta nos informar seu endereço de e-mail e enviaremos um link de redefinição de senha que permitirá que você escolha um novo.') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enviar link de redefinição de senha') }}
                 </x-button>
             </div>
         </form>
